872b79715374a9cb1c11fdd2e441ca7af72b2d16
lisicicd <44380882+lisicicd@users.noreply.github.com>
2018-11-01 16:54:37 +0100

    First Commit.

---
+++ taco-oblak/src/main/java/tacos/TacoOblakApplication.java
@@ -0,0 +1,12 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoOblakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoOblakApplication.class, args);
+	}
+}
